b4d8c483ccf66db00f738a47c1c93193961355d4
GrantPerkins <gcperk20@gmail.com>
2024-12-08 18:34:05 -0800

    Undo changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://kyleesk9s.com"
+  base: "https://grantperkins.github.io/PupWebsiteFrontend"
 })
